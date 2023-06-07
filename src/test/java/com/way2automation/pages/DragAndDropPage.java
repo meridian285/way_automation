488eab00999cbd4ccd29241225206597a41630bc
Aleksandr Elizarov <akelizarovav@gmail.com>
2023-06-07 09:50:38 +0300

    Добавить тест checkDragAndDrop()
    Добавить тестовый класс DragAndDropTests.java, page класс DragAndDropPage.java
    Добавлен класс CustomActions и SwitchTo

---
+++ src/test/java/com/way2automation/pages/DragAndDropPage.java
@@ -0,0 +1,37 @@
+package com.way2automation.pages;
+
+import com.way2automation.help.CustomActions;
+import com.way2automation.help.SwitchTo;
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+/**
+ * DragAndDropPage class
+ */
+
+public class DragAndDropPage extends BasePage {
+    // Локатор элемента 1
+    @FindBy(css = "div#draggable:first-of-type")
+    private WebElement draggableElement;
+    // Локатор элемента 2
+    @FindBy(xpath = "//div[@id='droppable']")
+    private WebElement droppableElement;
+    // Локатор фрейма
+    @FindBy(xpath = "//iframe[@src='droppable/default.html']")
+    private WebElement frame;
+
+    public DragAndDropPage() {
+        driver.get("http://way2automation.com/way2auto_jquery/droppable.php");
+        PageFactory.initElements(driver, this);
+    }
+
+    @Step("Перетащить элемент 1 на элемент 2")
+    public void dragAndDropTest() {
+        new SwitchTo(driver).switchToFrame(frame);
+        new CustomActions(driver).buildDragAndDrop(draggableElement, droppableElement);
+        Assert.assertEquals(droppableElement.getText(), "Dropped!");
+    }
+}

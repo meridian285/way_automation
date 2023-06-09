887e42c0802099e7f199467760ce58770f2505c4
Aleksandr Elizarov <akelizarovav@gmail.com>
2023-06-09 13:17:04 +0300

    Добавить пересоздал ветку с другим названием

--- src/test/java/com/way2automation/tests/BaseTest.java
+++ src/test/java/com/way2automation/tests/BaseTest.java
@@ -1,25 +1,53 @@
 package com.way2automation.tests;
 
+import com.way2automation.config.CapabilityFactory;
+import com.way2automation.config.DriverFactory;
 import com.way2automation.pages.BasePage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
-import java.util.concurrent.TimeUnit;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
 
 public class BaseTest {
     protected static WebDriver driver;
+    protected static ThreadLocal<RemoteWebDriver> remoteDriver = new ThreadLocal<>();
+    protected CapabilityFactory capabilityFactory = new CapabilityFactory();
 
-    @BeforeTest
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    // Настройки для локального запуска тестов
+    public void runLocal(String browser) {
+        driver = DriverFactory.getDrive(browser);
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         BasePage.setDriver(driver);
     }
 
-    @AfterTest
+    // Настройки для удаленного запуска тестов
+    public void runRemote(String browser) {
+        try {
+            remoteDriver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilityFactory.getCapabilities(browser)));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        driver = remoteDriver.get();
+        remoteDriver.get().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        remoteDriver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        BasePage.setDriver(remoteDriver.get());
+    }
+
+    // BeforeMethod с параметрами из testng.xml
+    @Parameters(value = {"browser", "environment"})
+    @BeforeMethod
+    public void setUp(String browser, String environment) {
+        if (environment.equals("remote")) {
+            runRemote(browser);
+        } else
+            runLocal(browser);
+    }
+
+    @AfterMethod
     public void tearDown() {
         driver.quit();
     }
